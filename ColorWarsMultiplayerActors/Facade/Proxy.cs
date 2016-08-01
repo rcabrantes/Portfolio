0c5a6995f401985960cea6a34e5911119913938b
rsinohara <rsinohara@hotmail.com>
2016-08-01 13:27:17 +0200

    First commit

---
+++ ColorWarsMultiplayerActors/Facade/Proxy.cs
@@ -0,0 +1,45 @@
+﻿using Akka.Actor;
+using ColorWarsMultiplayerActors.Actors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorWarsMultiplayerActors.Facade
+{
+    public static class Proxy
+    {
+        private static IProxyClient _client;
+        private static ActorSystem _system;
+
+        private static IActorRef _lobbyActor;
+
+
+        public static void ConnectClient(string connectionID,string userName)
+        {
+            if(_lobbyActor!=null)
+            {
+                _lobbyActor.Tell(new LobbyActor.ConnectMessage(connectionID,userName));
+            }
+        }
+
+        public static void Subscribe(IProxyClient client)
+        {
+            _client = client;
+
+            if(_system==null)
+            {
+                CreateSystem();
+            }
+        }
+
+        private static void CreateSystem()
+        {
+            _system = ActorSystem.Create("ColorWarsSystem");
+            _lobbyActor = _system.ActorOf<LobbyActor>("Lobby");
+
+
+        }
+    }
+}
--- Tests/Class1.cs
+++ Tests/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ColorWarsMultiplayerActors
+namespace Tests
 {
     public class Class1
     {

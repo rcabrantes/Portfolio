b1c26b214bfb6e170c018f8758114b402d6de20b
rsinohara <rsinohara@hotmail.com>
2016-08-01 11:41:12 +0200

    First commit

---
+++ Portfolio/Controllers/ColorWarsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ColorWarsController : Controller
+    {
+        // GET: ColorWars
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TestHubLogin()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portfolio/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNetCore.StaticFiles;
+
+[assembly: OwinStartupAttribute(typeof(Portfolio.Startup))]
+namespace Portfolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}

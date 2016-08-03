7aa5534955eeea1926abaecd4b908afc307bbac1
rsinohara <rsinohara@hotmail.com>
2016-08-03 16:02:20 +0200

    Fixed colors

---
+++ Portfolio/Content/Core/gameCell.js
@@ -0,0 +1,23 @@
+﻿"use strict";
+class gameCell {
+    //Color
+    //Owner
+
+
+
+    initRandom() {
+
+        this.color = Math.floor(Math.random() * 9) ;
+        this.owner = 0;
+        this.surrounded = false;
+    }
+
+    colorString(){
+        return getColorString(this.color);
+    }
+
+    toString() {
+        return this.color + '(' + this.owner + ')';
+    }
+
+}
